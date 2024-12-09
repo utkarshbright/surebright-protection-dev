ec6f135e8845bd24d6c8b22fd25c9c85b3e9ed14
yutkarsh.gupta5 <utkarsh.gupta@techstalwarts.com>
2024-12-09 11:50:42 +0000

    Changed integration setup class name and added logger

---
+++ Setup/InstallData.php
@@ -0,0 +1,44 @@
+<?php
+namespace Surebright\Integration\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+use Magento\Framework\Setup\InstallDataInterface;
+
+use Psr\Log\LoggerInterface;
+
+class InstallData implements InstallDataInterface{
+    /**
+     * @var ConfigBasedIntegrationManager
+     */
+
+    private $integrationManager;
+    protected $logger;
+
+    /**
+     * @param ConfigBasedIntegrationManager $integrationManager
+     */
+
+    public function __construct(ConfigBasedIntegrationManager $integrationManager, LoggerInterface $logger)
+    {
+        $this->integrationManager = $integrationManager;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        try{
+            $this->logger->debug("Initiating Surebright Integration setup.");
+            $this->integrationManager->processIntegrationConfig(['SureBright Product Protection']);
+            $this->logger->debug("Completed Surebright Integration setup.");
+        }catch(Exception $error){
+            $this->logger->debug("Logging error on Surebright Integration setup.");
+            $this->logger->debug($error);
+        }
+    }
+}
